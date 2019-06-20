c535ba7b3d9d06efffdb9d0dd2edac0ec743c7a1
Ben Martinson and Jess Doss <ben@>
2019-06-20 11:30:44 -0700

    add animation effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
-
-
 function Player(symbol){
   this.symbol = symbol
   this.spotsFilled = [];
 }
-
+function  Computer(symbol){
+  this.symbol = symbol
+  this.spotsFilled = [];
+}
 Player.prototype.fillSpot = function(squareId){
   this.spotsFilled.push(squareId)
 }
-
+Computer.prototype.fillSpot = function(squareId){
+   this.spotsFilled.push(squareId)
+}
+Computer.prototype.checkForWin = function(arr1, arr2){
+  var spotsFilled = this.spotsFilled
+  var result = null;
+  spotsNext.forEach(function(spot){
+    var twoNext = spot[0]
+    var winningSpot = spot[1]
+    if(arr1.includes(twoNext[0]) && arr1.includes(twoNext[1])){
+      if(!arr2.includes(winningSpot)){
+        console.log("here");
+        result = winningSpot
+      }
+    }
+  })
+  return result;
+}
+Computer.prototype.fillNonCorner = function(){
+  return nonCorners[Math.floor(Math.random() * Math.floor(4))]
+}
 function emptyBoard(){
   for(let i = 1; i <= 9; ++i){
     $("#g-" + i).text("")
@@ -26,9 +47,11 @@ function printBoard(){
 }
 
 var player1 = new Player("X");
-var player2 = new Player("O");
+var player2 = new Computer("O");
 var turn = player1;
 var notTurn = player2;
+var spotsNext = [ [[5,7], 3] , [[1,2],3], [[4,5],6], [[7,8],9], [[3,6],9], [[1,4],7], [[2,5],8], [[3, 5], 7], [[2,3],1],[[5,6],4],[[8,9],7],[[6,9],3],[[4,7],1],[[5,8],2],[[1,7],4],[[1,3],2],[[1,9],5],[[3,9],6],[[3,7],5],[[4,6],5],[[7,9],8],[[2,8],5]];
+var nonCorners = [2,4,6,8];
 
 function switchTurn(){
   var temp = turn;
@@ -57,17 +80,15 @@ function checkWin(player){
   }
 }
 
-function checkSpot(arr1, arr2, id){
+
+function checkSpot(id){
   var found = false;
-  arr1.forEach(function(spot){
-    console.log(spot);
-    console.log(id);
+  player1.spotsFilled.forEach(function(spot){
     if(spot === id) {
-      console.log("equal");
-      found = true
+          found = true
     }
   })
-  arr2.forEach(function(spot2){
+  player2.spotsFilled.forEach(function(spot2){
     if(spot2 === id) {
       found = true
     }
@@ -75,38 +96,75 @@ function checkSpot(arr1, arr2, id){
   return found;
 }
 
+//computer move
+function autoFill(){
+  var squareChoice;
+  var arr1 = player1.spotsFilled
+  var arr2 = player2.spotsFilled
+  //return player2.checkForWin(arr2, arr1) || player2.checkForWin(arr1, arr2)
+
+  squareChoice = player2.checkForWin(arr2, arr1)
+  if(squareChoice) return squareChoice
+  squareChoice = player2.checkForWin(arr1, arr2)
+  if(squareChoice) return squareChoice
+    // if(!squareChoice){
+    // squareChoice = player2.checkForWin(player1.spotsFilled, player2.spotsFilled)
+    if(!squareChoice){
+      var count = 0
+      do{
+
+        squareChoice = (Math.floor(Math.random() * Math.floor(9)))+1
+      } while(checkSpot(squareChoice));
+    }
+  return squareChoice
+}
+
+function check(){
+
+  if(checkWin(turn)){
+    $("#winDisplay").show()
+    $("#winPlayer").text(turn.symbol+"'s have ")
+    return true
+  }
+  if(player1.spotsFilled.length === 5 || player2.spotsFilled.length === 5){
+    $("#winDisplay").show()
+    $("#winPlayer").text("Nobody has ")
+    return true
+  }
+  switchTurn();
+  console.log("here");
+  return false
+
+}
+
 $(document).ready(function(){
   printBoard();
 
   $(".grid-item").click(function(){
-
     var clickedSquare = parseInt($(this)[0].id);
-    if(!checkSpot(player1.spotsFilled, player2.spotsFilled, clickedSquare)){
-      console.log("here");
+    if(!checkSpot(clickedSquare)){
       turn.fillSpot(clickedSquare)
       printBoard();
-      if(checkWin(turn)){
-        $("#winDisplay").show()
-        $("#winPlayer").text(turn.symbol+"'s have ")
-        return
+      if(check()) return
+
+      var squareChoice;
+      if (player1.spotsFilled.length === 1 && player1.spotsFilled[0] === 5){
+        console.log(player1.spotsFilled.length)
+        squareChoice = player2.fillNonCorner()
+      } else {
+        squareChoice = autoFill();
       }
-      if(player1.spotsFilled.length === 5 || player2.spotsFilled.length === 5){
-        $("#winDisplay").show()
-        $("#winPlayer").text("Nobody has ")
-        return
-      }
-      switchTurn();
-    } else {
-      console.log("spot filled");
+      turn.fillSpot(squareChoice);
+      printBoard();
+
+      if(check()) return
     }
+  })
 
-    $(".refresh").click(function(event) {
-      $("#winDisplay").hide();
-      player1.spotsFilled = [];
-      console.log(player1.spotsFilled);
-      player2.spotsFilled = [];
-      emptyBoard();
-    })
-
+  $(".refresh").click(function(event) {
+    $("#winDisplay").hide();
+    player1.spotsFilled = [];
+    player2.spotsFilled = [];
+    emptyBoard();
   })
 })
