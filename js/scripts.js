05b7738bae706778b0c62ce495630096aacf6aeb
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-06-23 12:42:30 -0700

    completed fork blocking

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ var notTurn = player2;
 var spotsNext = [ [[5,7], 3] , [[1,2],3], [[4,5],6], [[7,8],9], [[3,6],9], [[1,4],7], [[2,5],8],
                 [[3, 5], 7], [[2,3],1],[[5,6],4],[[8,9],7],[[6,9],3],[[4,7],1],[[5,8],2],[[1,7],4],
                 [[1,3],2],[[1,9],5],[[3,9],6],[[3,7],5],[[4,6],5],[[7,9],8],[[2,8],5], [[1,5],9]];
+
+var edgesBlockFork = [[[2,4],1], [[2,6],3], [[4,8],7],[[6,8],9]]
 var nonCorners = [2,4,6,8];
 var cornerSpots = [1,3,7,9];
 var oppositeCorner = [9,7,3,1];
@@ -90,10 +92,25 @@ Computer.prototype.blockFork = function(){
           result = edge;
       })
     }
+    else if(edgeSpots.includes(player1.spotsFilled[0]) && edgeSpots.includes(player1.spotsFilled[1])){
+      result = player2.blockEdgeFork();
+    }
   }
   return result;
 
 }
+
+Computer.prototype.blockEdgeFork = function(){
+  var res = 0;
+  edgesBlockFork.forEach(function(ele){
+    if(player1.spotsFilled.includes(ele[0][0]) && player1.spotsFilled.includes(ele[0][1])){
+      res = ele[1]
+    }
+  })
+  return res;
+}
+
+
 
 
 function emptyBoard(){
